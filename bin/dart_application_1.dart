953ae8bc0ee301d17dd0e3bce5b7abee511d119d
Raul <raulcastidev2004@gmail.com>
2024-02-05 18:32:32 +0100

    Primera subida en formato ingles

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,12 @@
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main(List<String> arguments) {
+  // Obtener la fecha y hora actual
+  DateTime now = DateTime.now();
+  // Formatear la fecha y hora
+  String formattedDateTime =
+      "${now.year}-${now.month}-${now.day} ${now.hour}:${now.minute}:${now.second}";
+  // Imprimir el mensaje con la fecha y hora
+  print('Hello world: ${dart_application_1.calculate()}! ($formattedDateTime)');
+}
